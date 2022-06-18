eadd1f5fc383468bd235f58af809574d4e04c7d7
Kisei Tanaka <kisei.tanaka@gmail.com>
2022-06-17 16:15:37 -1000

    Update 4_Plot_LEHI_Maps_1985-2021_CRW.R

--- scripts/4_Plot_LEHI_Maps_1985-2021_CRW.R
+++ scripts/4_Plot_LEHI_Maps_1985-2021_CRW.R
@@ -39,71 +39,40 @@ ipcc_col <- c(rgb(103, 0, 31, maxColorValue = 255, alpha = 255),
 
 map = function(mode){
   
-  load("outputs/HadI_0.96667_LEHI_1980-1989.RData"); hadi1 = anom; hadi1$source = "HadISST v1.1"; hadi1$period = "1980-1989"
-  load("outputs/HadI_0.96667_LEHI_1990-1999.RData"); hadi2 = anom; hadi2$source = "HadISST v1.1"; hadi2$period = "1990-1999"
-  load("outputs/HadI_0.96667_LEHI_2000-2009.RData"); hadi3 = anom; hadi3$source = "HadISST v1.1"; hadi3$period = "2000-2009"
-  load("outputs/HadI_0.96667_LEHI_2010-2019.RData"); hadi4 = anom; hadi4$source = "HadISST v1.1"; hadi4$period = "2010-2019"
-  
-  load("outputs/COBE_0.96667_LEHI_1980-1989.RData"); cobe1 = anom; cobe1$source = "COBE v2"; cobe1$period = "1980-1989"
-  load("outputs/COBE_0.96667_LEHI_1990-1999.RData"); cobe2 = anom; cobe2$source = "COBE v2"; cobe2$period = "1990-1999"
-  load("outputs/COBE_0.96667_LEHI_2000-2009.RData"); cobe3 = anom; cobe3$source = "COBE v2"; cobe3$period = "2000-2009"
-  load("outputs/COBE_0.96667_LEHI_2010-2019.RData"); cobe4 = anom; cobe4$source = "COBE v2"; cobe4$period = "2010-2019"
+  load("outputs/CRW_0.96667_LEHI_1985-1994.RData"); crw1 = anom; crw1$source = "CRW v1.1"; crw1$period = "1985-1994"
+  load("outputs/CRW_0.96667_LEHI_1995-2004.RData"); crw2 = anom; crw2$source = "CRW v1.1"; crw2$period = "1995-2004"
+  load("outputs/CRW_0.96667_LEHI_2005-2014.RData"); crw3 = anom; crw3$source = "CRW v1.1"; crw3$period = "2005-2014"
+  load("outputs/CRW_0.96667_LEHI_2015-2021.RData"); crw4 = anom; crw4$source = "CRW v1.1"; crw4$period = "2015-2021"
   
   if (mode == "annual") {
     
-    anom = rbind(hadi1, hadi2, hadi3, hadi4, 
-                 cobe1, cobe2, cobe3, cobe4)
-    
-    anom$source = factor(anom$source, levels = c("HadISST v1.1", "COBE v2"))
+    anom = rbind(crw1, crw2, crw3, crw4)
     
     anom %>% 
-      # mutate(sum = range01(sum)) %>%
       group_by(period) %>% 
       summarise(mean = mean(sum)/120)
     
     (p = anom %>% 
-        # mutate(sum = range01(sum)) %>% 
         group_by(x, y, period) %>% 
         summarise(sum = mean(sum)/120) %>% 
         ggplot() + 
-        # geom_raster(aes(x, y, fill = sum)) +
-        geom_tile(aes(x, y, fill = sum), width = 1, height = 1) + 
+        geom_tile(aes(x, y, fill = sum), width = 1, height = 1) +
         annotation_map(map_data("world"), fill = "gray50", colour = "gray20") +
-        scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c(0, 1), breaks = c(0, 0.5, 1)) +
-        scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
-        scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
+        scale_fill_gradientn(colors = rev(ipcc_col), "") +
+        # scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
+        # scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
         facet_wrap(~period, nrow = 2) +
         # coord_fixed() + 
-        # coord_sf(xlim = range(anom$x), ylim = range(anom$y)) +
-        coord_map("ortho", orientation = c(0, 180, 0)) + #normal
+        coord_sf(xlim = range(anom$x), ylim = range(anom$y)) +
+        # coord_map("ortho", orientation = c(0, median(anom$x), 0)) + #normal
         # coord_map(projection = "mercator") +
         theme(axis.title = element_blank(),
               axis.ticks = element_blank(), 
               axis.text = element_blank(),
-              legend.position = "bottom"))
-    
-    png(paste0("outputs/HadI_COBE_annual_map_v1_", percentile, ".png"), height = 6.5, width = 6, units = "in", res = 500)
-    print(p)
-    dev.off()
-    
-    (p = anom %>% 
-        group_by(x, y, period, source) %>% 
-        summarise(sum = median(sum)/120) %>% 
-        ggplot() +
-        geom_tile(aes(x, y, fill = sum), width = 1, height = 1) + 
-        annotation_map(map_data("world"), fill = "gray50", colour = "gray20", size = 0.5) +
-        scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c(0, 1), breaks = c(0, 0.5, 1)) +
-        scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
-        scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
-        facet_grid(source ~ period) +
-        coord_fixed() +
-        theme(axis.title = element_blank(),
-              axis.text = element_blank(),
-              axis.ticks = element_blank(),
               legend.position = "bottom",
               legend.justification = c(1,0)))
     
-    png(paste0("outputs/HadI_COBE_annual_map_v2_", percentile, ".png"), height = 6.5, width = 12, units = "in", res = 500)
+    png(paste0("outputs/CRW_annual_map_v1_", percentile, ".png"), height = 6.5, width = 6, units = "in", res = 500)
     print(p)
     dev.off()
     
@@ -111,8 +80,7 @@ map = function(mode){
   
   if (mode == "seasonal_difference") {
     
-    anom = rbind(hadi1, hadi2, hadi3, hadi4, 
-                 cobe1, cobe2, cobe3, cobe4)
+    anom = rbind(crw1, crw2, crw3, crw4)
     
     anom$source = factor(anom$source, levels = c("HadISST v1.1", "COBE v2"))
     
@@ -131,19 +99,20 @@ map = function(mode){
     
     (p = seasonal_differnece %>% 
         ggplot() +
-        geom_tile(aes(x, y, fill = diff), width = 1, height = 1) + 
+        geom_tile(aes(x, y, fill = diff), width = 0.5, height = 0.5) + 
         annotation_map(map_data("world"), fill = "gray50", colour = "gray20", size = 0.5) +
-        scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c(-1, 1), breaks = c(-1, 0, 1)) +
-        scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
-        scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
-        coord_map("ortho", orientation = c(0, 180, 0)) + #normal
-        theme(#axis.title = element_blank(),
-          #axis.text = element_blank(),
-          #axis.ticks = element_blank(),
-          legend.position = "bottom",
-          legend.justification = c(1,0)))
+        scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c( max(abs(seasonal_differnece$diff))*-1,
+                                                                     max(abs(seasonal_differnece$diff)))) +
+        # scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
+        # scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
+        # coord_map("ortho", orientation = c(0, 180, 0)) + #normal
+        theme(axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              legend.position = "bottom",
+              legend.justification = c(1,0)))
     
-    png(paste0("outputs/HadI_COBE_annual_map_v3_", percentile, ".png"), height = 6, width = 6, units = "in", res = 500)
+    png(paste0("outputs/CWR_annual_map_v3_", percentile, ".png"), height = 6, width = 6, units = "in", res = 500)
     print(p)
     dev.off()
     
@@ -152,13 +121,13 @@ map = function(mode){
   if (mode == "combine") {
     
     #all periods
-    annual = rbind(hadi1, hadi2, hadi3, hadi4, cobe1, cobe2, cobe3, cobe4)
+    annual = rbind(crw1, crw2, crw3, crw4)
     annual$sum = annual$sum/120
     annual$season = "Annual"
     annual = annual[, c("x", "y", "sum", "source", "period", "season")]
     
     #seasonals
-    anom = rbind(hadi1, hadi2, hadi3, hadi4, cobe1, cobe2, cobe3, cobe4)
+    anom = rbind(crw1, crw2, crw3, crw4)
     season_1 = anom[,c("x", "y", "jan", "feb", "mar", "source", "period")]; season_1$season = "Jan-Mar"
     season_2 = anom[,c("x", "y", "jul", "aug", "sep", "source", "period")]; season_2$season = "Jul-Sep"
     season_1$sum = rowSums(season_1[3:5])
@@ -174,11 +143,11 @@ map = function(mode){
     (p = ggplot(anom) + 
         geom_tile(aes(x, y, fill = sum), width = 1, height = 1) + 
         annotation_map(map_data("world"), fill = "gray50", colour = "gray20", size = 0.5) +
-        scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c(0,1), breaks = c(0, 0.5, 1)) +
-        scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
-        scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
+        scale_fill_gradientn(colors = rev(ipcc_col), "") +
+        # scale_x_continuous(expand = c(-0.005, 15), "", limits = range(anom$x)) +
+        # scale_y_continuous(expand = c(-0.005, 15), "", limits = range(anom$y)) +
         # coord_fixed() +
-        coord_map("ortho", orientation = c(0, 180, 0)) + #normal
+        # coord_map("ortho", orientation = c(0, 180, 0)) + #normal
         facet_grid(season ~ period) +
         theme(axis.title = element_blank(),
               axis.text = element_blank(),
@@ -186,7 +155,7 @@ map = function(mode){
               legend.position = "bottom",
               legend.justification = c(1,0)))
     
-    png(paste0("outputs/HadI_COBE_annual_map_v4_", percentile, ".png"), height = 7, width = 10, units = "in", res = 500)
+    png(paste0("outputs/CRW_annual_map_v4_", percentile, ".png"), height = 7, width = 10, units = "in", res = 500)
     print(p)
     dev.off()
     
