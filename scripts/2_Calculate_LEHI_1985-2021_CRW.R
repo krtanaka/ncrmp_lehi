fd50088a95d8174fae36aa0d01c2260e7ca42894
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-05-24 13:05:07 -1000

    Update 2_Calculate_LEHI_1985-2021_CRW.R

--- scripts/2_Calculate_LEHI_1985-2021_CRW.R
+++ scripts/2_Calculate_LEHI_1985-2021_CRW.R
@@ -6,31 +6,43 @@ library(sf)
 library(rgdal)
 library(dplyr)
 library(maps)
+library(foreach)
+library(doParallel)
+library(pbapply)
 
 rm(list = ls())
 
-percentile = 0.96667 #based on 30 years baseline (1955-1984)
+percentile = 0.96667 #based on 30 years baseline (1985-2014, one of CMIP6)
 
 calculate_anomalies = function(period){
   
   # period = "2015-2021"
-
-  load("data/CRW_1985-2021.RData"); df = monthly_CRW; names(df)
   
+  load("G:/CRW_SST/CRW_1985-2022.RData")
+  df = monthly_CRW; names(df)
+  df = df[complete.cases(df), ]
+
   # set baseline Jan 1985 - Dec 2014, 30 years
-  Baseline <-  df[,1:362]
+  Baseline <-  df[,1:360]
   names(Baseline) #look at time steps
   
   # set target period
   if (period == "1985-1994") Target <- df[, 1:122] #Jan 1985 - Dec 1994
-  if (period == "1995-2004") Target <- df[,c(1:2, 123:242)] #Jan 1995 - Dec 2004
-  if (period == "2005-2014") Target <- df[,c(1:2, 243:362)] #Jan 2005 - Dec 2014
-  if (period == "2015-2021") Target <- df[,c(1:2, 363:446)] #Jan 2015 - Dec 2021
+  if (period == "1995-2004") Target <- df[,c(1:2, 123:241)] #Jan 1995 - Dec 2004
+  if (period == "2005-2014") Target <- df[,c(1:2, 242:360)] #Jan 2005 - Dec 2014
+  if (period == "2015-2021") Target <- df[,c(1:2, 361:444)] #Jan 2015 - Dec 2021
   
-  ll_anom = NULL
+  # Create an empty list to store the results
+  ll_anom <- list()
+  
+  # Define the parallel backend
+  cl <- makeCluster(detectCores()-2) # Adjust the number of cores as per your system
+  
+  # Register the parallel backend
+  registerDoParallel(cl)
   
   # calculate lehi at every lot/lon grid cell
-  for (ll in 1:dim(Baseline)[1]) { 
+  ll_anom <- foreach(ll = 1:dim(Baseline)[1], .combine = rbind) %dopar% {
     
     # ll = 5
     
@@ -65,12 +77,17 @@ calculate_anomalies = function(period){
       
     }
     
-    ll_anom = rbind(ll_anom, monthly_anom)
+    monthly_anom
     
   }
   
+  # Stop the parallel backend
+  stopCluster(cl)
+  
+  ll_anom = as.data.frame(ll_anom)
+  
   colnames(ll_anom) = c("jan", "feb", "mar", "apr", "may", "jun",
-                        "jul", "aug", "sep", "oct", "nov", "dec")
+                  "jul", "aug", "sep", "oct", "nov", "dec")
   
   anom = cbind(Target[1:2], ll_anom)
   
