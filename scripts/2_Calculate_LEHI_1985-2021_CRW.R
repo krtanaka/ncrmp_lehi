3d3fbc1098579cd5aae2a8401c9d5ad37be511b8
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-05-24 17:28:49 -1000

    Update 2_Calculate_LEHI_1985-2021_CRW.R

--- scripts/2_Calculate_LEHI_1985-2021_CRW.R
+++ scripts/2_Calculate_LEHI_1985-2021_CRW.R
@@ -18,22 +18,20 @@ calculate_anomalies = function(period){
   
   # period = "2015-2021"
   
-  load("G:/CRW_SST/CRW_1985-2022.RData")
-  df = monthly_CRW; names(df)
-  df = df[complete.cases(df), ]
+  load("G:/CRW_SST/CRW_1985-2022.RData"); names(df)
 
   # set baseline Jan 1985 - Dec 2014, 30 years
-  Baseline <-  df[,1:360]
+  Baseline <-  df[,1:362]
   names(Baseline) #look at time steps
   
   # set target period
   if (period == "1985-1994") Target <- df[, 1:122] #Jan 1985 - Dec 1994
-  if (period == "1995-2004") Target <- df[,c(1:2, 123:241)] #Jan 1995 - Dec 2004
-  if (period == "2005-2014") Target <- df[,c(1:2, 242:360)] #Jan 2005 - Dec 2014
-  if (period == "2015-2021") Target <- df[,c(1:2, 361:444)] #Jan 2015 - Dec 2021
+  if (period == "1995-2004") Target <- df[,c(1:2, 123:242)] #Jan 1995 - Dec 2004
+  if (period == "2005-2014") Target <- df[,c(1:2, 243:362)] #Jan 2005 - Dec 2014
+  if (period == "2015-2021") Target <- df[,c(1:2, 363:446)] #Jan 2015 - Dec 2021
   
   # Define the parallel backend
-  cl <- makeCluster(detectCores()-2) # Adjust the number of cores as per your system
+  cl <- makeCluster(detectCores()-2)
   
   # Register the parallel backend
   registerDoParallel(cl)
@@ -49,7 +47,7 @@ calculate_anomalies = function(period){
     
     for (m in 1:12) { # every month
       
-      # m = 8
+      # m = 12
       
       interval = seq(m + 2, dim(Baseline)[2], by = 12)
       
@@ -62,7 +60,7 @@ calculate_anomalies = function(period){
       # hist(baseline, breaks = 100, col = matlab.like(100), lty = "blank")
       # abline(v = q)
       
-      interval = seq(m+2, dim(Target)[2], by = 12)
+      interval = seq(m + 2, dim(Target)[2], by = 12)
       
       present = Target[ll, c(interval)]
       present = t(present)
@@ -84,7 +82,7 @@ calculate_anomalies = function(period){
   ll_anom = as.data.frame(ll_anom)
   
   colnames(ll_anom) = c("jan", "feb", "mar", "apr", "may", "jun",
-                  "jul", "aug", "sep", "oct", "nov", "dec")
+                        "jul", "aug", "sep", "oct", "nov", "dec")
   
   anom = cbind(Target[1:2], ll_anom)
   
