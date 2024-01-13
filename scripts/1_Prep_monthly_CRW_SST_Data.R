f6866dce85afa9c04a4d84a1a4f85b60b192f861
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-01-12 15:18:09 -1000

    Update 1_Prep_monthly_CRW_SST_Data.R

--- scripts/1_Prep_monthly_CRW_SST_Data.R
+++ scripts/1_Prep_monthly_CRW_SST_Data.R
@@ -13,18 +13,71 @@ library(rgdal)
 library(dplyr)
 library(readr)
 library(doParallel)
+library(data.table)
 
 # cores = detectCores()-2
 # registerDoParallel(cores = cores)
 
-nc_list = list.files(path = "G:/SST/CRW_SST/", pattern = "\\.nc$", full.names = T); nc_list
+# I had to download CRW SST files from both Coastwatch and Oceanwatch URLs separately. The Coastwatch URL was missing files for February 2022 and November 2023, so I obtained them from the Oceanwatch URLs. - KRT 2024/01/12
 
-df = stack(nc_list, varname = "sea_surface_temperature")
-df = df[[1:459]]
-df <- df %>% rasterToPoints() %>% data.frame()
-df <- df %>% select(-matches("00\\.00\\.00\\.2"))
-colnames(df)[3:dim(df)[2]] <- substring(colnames(df)[3:dim(df)[2]] , 1, 8)
-df
+# CoastWatch files
+nc_list_cw = list.files(path = "G:/SST/CRW_SST/coastwatch/", pattern = "\\.nc$", full.names = T); nc_list_cw
+
+# Set up parallel processing
+cores <- detectCores()
+cl <- makeCluster(cores/2)
+registerDoParallel(cl)
+
+e = extent(143, 207, -16, 30) # pacific NCRMP region
+
+# set base lat lon points because of potential mismatch later
+base_latlon = rast(nc_list_cw[1], subds = "sea_surface_temperature") %>% 
+  rotate(left = FALSE) %>% 
+  crop(e) %>% 
+  terra::as.data.frame(xy = T) %>% 
+  select(x, y)
+
+# stacked separately because of changes in crs half way
+df1 <- foreach(i = 1:430, .combine = cbind, .packages = c("terra", "dplyr", "raster")) %dopar% {
+  
+  # i = 1
+  
+  df_i = rast(nc_list_cw[i], subds = "sea_surface_temperature")
+  df_i <- rotate(df_i, left = FALSE) 
+  df_i = crop(df_i, e)
+  df_i <- df_i %>% terra::as.data.frame(xy = T)
+  df_i <- df_i %>% dplyr::select(-matches("00\\.00\\.00\\.2"))
+  colnames(df_i)[3:dim(df_i)[2]] <- paste0("X", substring(nc_list_cw[i], 54, 59))
+  
+  if (i != 1) {
+  df_i = left_join(base_latlon, df_i)
+  }
+  
+  df_i
+  
+}
+
+df1 <- foreach(i = 431:466, .combine = cbind, .packages = c("terra", "dplyr", "raster")) %dopar% {
+  
+  # i = 1
+  
+  df_i = rast(nc_list_cw[i], subds = "sea_surface_temperature")
+  df_i <- rotate(df_i, left = FALSE) 
+  df_i = crop(df_i, e)
+  df_i <- df_i %>% terra::as.data.frame(xy = T)
+  df_i <- df_i %>% dplyr::select(-matches("00\\.00\\.00\\.2"))
+  colnames(df_i)[3:dim(df_i)[2]] <- paste0("X", substring(nc_list_cw[i], 54, 59))
+  
+  if (i != 1) {
+    df_i = left_join(base_latlon, df_i)
+  }
+  
+  df_i
+  
+}
+
+# Stop parallel processing
+stopCluster(cl)
 
 # r <- foreach(isl = 1:length(nc_list), .combine = rbind, .packages = c("raster", "dplyr")) %dopar% {
 #   
